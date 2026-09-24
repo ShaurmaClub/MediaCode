e3267c2555b44573794bb9997e8cb9f6dbf31c0b
ShaurmaClub <242012393+ShaurmaClub@users.noreply.github.com>
2026-09-24 18:20:01 +0300

    chore: remove temporary E2E and patch scripts

--- fix.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let c = fs.readFileSync('src/pages/Login.jsx', 'utf8');
-c = c.replace("toast.success(С возвращением, !);", "toast.success(`С возвращением, ${data.user.first_name}!`);");
-fs.writeFileSync('src/pages/Login.jsx', c);
